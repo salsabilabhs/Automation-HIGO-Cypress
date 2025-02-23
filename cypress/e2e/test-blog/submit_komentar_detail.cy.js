69f3b9eb34106988c1fc4cc2151bbf5e68011963
LAPTOP-66B9Q4LL\Owner <lala@maxxitani.id>
2025-02-23 07:38:13 +0700

    tc blog

--- cypress/e2e/test-blog/submit_komentar_detail.cy.js
+++ cypress/e2e/test-blog/submit_komentar_detail.cy.js
@@ -2,14 +2,14 @@ import detailarticle from "../../support/blog/detailarticle_blog"
 import dashboard from "../../support/blog/dashboard_blog"
 
 describe('User submit Komentar on article detail', () => {
-    beforeEach(()=> {
-        cy.visit('/alasan-personalized-marketing-penting-di-tahun-2025')
-    })
 
     it('User successfully submit Komentar with valid credentials on article detail', () => {
         cy.fixture('detailarticle_blog').then((data) => {
+            cy.origin('https://blog.higo.id', () => {
+                cy.visit('/alasan-personalized-marketing-penting-di-tahun-2025', { failOnStatusCode: false })
                 cy.xpath(detailarticle.field_nama).type(data.nama_valid)
                 cy.xpath(detailarticle.field_nama).type(data.komentar_valid)
+            })
         })
     })
 })
